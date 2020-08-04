84bbd69205832f035e32b71b00ca55233d000360
Michael Rabon <nobar86@gmail.com>
2020-08-04 00:44:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
-array = ["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"]
+collection = ["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"]
 def my_collect(collection)
   i = 0
-  array = []
+  collection = []
   my_collect(collection) do |lang|
     lang.upcase
   while i < array.length
-    array << yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
   languages
