c32c18b474947d14f7fa20a4e32525b318a86123
Michael Rabon <nobar86@gmail.com>
2020-08-04 00:46:47 +0000

    Automatically backed up by Learn

---
+++ lib/my_collect.rb
@@ -0,0 +1,13 @@
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  
+   name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
+
+my_collect(list) {|i| i.split(" ").first}
